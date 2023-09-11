5698030695dd5dc5ada48ab00b0711b8067fa2fb
as8811114 <as881114@gmail.com>
2023-09-11 18:00:27 +0800

    add drag view

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = { hasFile: false, imgSrc: "", scale: 0 };
+    this.state = {
+      originPoint: { x: 0, y: 0 },
+      oldOrignPoint: { x: 0, y: 0 },
+      oldPoint: { x: 0, y: 0 },
+      offSet: { x: 0, y: 0 },
+      hasFile: false,
+      imgSrc: "",
+      scale: 0,
+      isDragging: false,
+    };
   }
   drawImageInCanvas(type, imgUrl) {
     const Canvas = document.getElementById("canvas");
@@ -28,11 +37,37 @@ class App extends Component {
       //set image height and width
       const h = img.height * this.state.scale;
       const w = img.width * this.state.scale;
+      if (type === "init") {
+        this.setState({
+          originPoint: {
+            x: Canvas.width / 2 - w / 2,
+            y: Canvas.height / 2 - h / 2,
+          },
+          oldOrignPoint: {
+            x: Canvas.width / 2 - w / 2,
+            y: Canvas.height / 2 - h / 2,
+          },
+        });
+      }
+      //if image's bound is outside of canvas's bound then user can drag the image
+      else if (
+        type === "drag" &&
+        (this.state.originPoint.x < 0 ||
+          this.state.originPoint.y < 0 ||
+          this.state.originPoint.x + w > Canvas.width ||
+          w > this.state.originPoint.y + Canvas.width)
+      ) {
+        this.setState({
+          originPoint: {
+            x: this.state.oldOrignPoint.x + this.state.offSet.x,
+            y: this.state.oldOrignPoint.y + this.state.offSet.y,
+          },
+        });
+      }
 
       //set image origin coordinate
-      const originX = Canvas.width / 2 - w / 2;
-      const originY = Canvas.height / 2 - h / 2;
-
+      const originX = this.state.originPoint.x;
+      const originY = this.state.originPoint.y;
       //clean last image
       canvasContext2D.clearRect(0, 0, Canvas.width, Canvas.height);
 
@@ -58,6 +93,31 @@ class App extends Component {
             border: "1px black solid",
             width: "70vw",
             maxWidth: "640px",
+          }}
+          onMouseDown={(e) => {
+            this.setState({
+              oldPoint: { x: e.clientX, y: e.clientY },
+              isDragging: true,
+            });
+          }}
+          onMouseMove={(e) => {
+            if (this.state.isDragging) {
+              const currentPoint = { x: e.clientX, y: e.clientY };
+              const offSetX = currentPoint.x - this.state.oldPoint.x;
+              const offSetY = currentPoint.y - this.state.oldPoint.y;
+              this.setState({ offSet: { x: offSetX, y: offSetY } });
+              this.drawImageInCanvas("drag", this.state.imgSrc);
+            }
+          }}
+          onMouseUp={(e) => {
+            this.setState({
+              isDragging: false,
+              offSet: { x: 0, y: 0 },
+              oldOrignPoint: {
+                x: this.state.originPoint.x,
+                y: this.state.originPoint.y,
+              },
+            });
           }}
         ></canvas>
         {!this.state.hasFile && (
